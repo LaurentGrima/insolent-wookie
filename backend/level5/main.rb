08dc2281600b788e8b5e8226da62adf5fc6d3b7e
LaurentGrima <grima.laurent@gmail.com>
2015-09-21 20:27:02 +0200

    level5: Python to Ruby

---
+++ backend/level5/main.rb
@@ -0,0 +1,37 @@
+require "json"
+require_relative "models/car"
+require_relative "models/rental"
+
+file = File.read('data.json')
+input_data = JSON.parse(file)
+
+# Create a car hash from input data to access cars by id.
+cars = {}
+input_data['cars'].each do |car|
+  cars[car['id']] = Car.new(car['id'], car['price_per_day'], car['price_per_km'])
+end
+
+# Create the list of rentals to be outputted.
+rentals = []
+input_data['rentals'].each do |input_rental|
+
+  output_rental = Rental.new(
+    input_rental['id'],
+    cars[input_rental['car_id']],
+    input_rental['start_date'],
+    input_rental['end_date'],
+    input_rental['distance'],
+    input_rental['deductible_reduction'],
+    )
+
+  item = {
+    :id => output_rental.id,
+    :actions => output_rental.get_actions
+    }
+  rentals << item
+end
+
+# Output the json file.
+File.open("_output.json", "w") do |f|
+  f.write({'rentals' => rentals }.to_json)
+end
